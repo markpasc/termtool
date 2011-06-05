0cd561d2f7073f99baad1f33e854de70f81449ac
Mark Paschal <markpasc@markpasc.org>
2011-06-05 11:47:17 -0700

    boilerplate

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
-from setuptools import setup
+from distutils.core import setup
+
+
+long_description = """
+`termtool` helps you write subcommand-based command line tools in Python. It collects several Python libraries into a declarative syntax:
+
+* :mod:`argparse`, the argument parsing module with subcommand support provided in the standard library in Python 2.7 and later.
+* `prettytable <http://code.google.com/p/python-progressbar/>`_, an easy module for building tables of information.
+* `progressbar <http://code.google.com/p/python-progressbar/>`_, a handy module for displaying progress bars.
+* :mod:`logging`, the simple built-in module for logging messages.
+"""
+
 
 setup(
     name='termtool',
     version='1.0',
-    packages=[],
-    include_package_data=True,
-    modules=['termtool.py'],
+    description='Declarative terminal tool programming',
+    author='Mark Paschal',
+    author_email='markpasc@markpasc.org',
+    url='https://github.com/markpasc/termtool',
 
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+
+    packages=[],
+    py_modules=['termtool'],
     requires=['argparse', 'PrettyTable', 'progressbar'],
-    install_requires=['argparse', 'PrettyTable', 'progressbar'],
 )
