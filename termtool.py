8269dac73718f117c5c0029fe42d69e5edc9da3a
Mark Paschal <markpasc@markpasc.org>
2013-01-23 12:24:04 -0500

    Add some docstrings

--- termtool.py
+++ termtool.py
@@ -12,7 +12,19 @@ __version__ = '1.1dev'
 
 
 def subcommand(*args, **kwargs):
-    def decor(fn):
+    """Decorate an instance method as a tool subcommand.
+
+    The function's own name is used as the subcommand name for the terminal
+    user to type. To use a different one, specify the name as the first
+    positional argument or the ``name`` kwarg to the `subcommand()` decorator.
+
+    Any remaining keyword arguments are passed to the `add_parser()` method
+    declaring the subcommand, so arguments to the `argparse.ArgumentParser`
+    constructor are valid.
+
+    """
+    def _decor(fn):
+        """Tag the decorated function with the subcommand settings to use later."""
         pargs = list(args)
         try:
             name = pargs.pop(0)
@@ -23,18 +35,28 @@ def subcommand(*args, **kwargs):
                 name = fn.__name__
         fn._subcommand = (name, pargs, kwargs)
         return fn
-    return decor
+    return _decor
 
 
 def argument(*args, **kwargs):
-    def decor(fn):
+    """Declare an argument to the decorated class or instance method.
+
+    The parameters to the `argument()` decorator should specify an argument
+    definition as if you were calling `Parser.add_argument()`.
+
+    If decorating a class, the specified argument is a global argument
+    available to all subcommands.
+
+    """
+    def _decor(fn):
+        """Tag the decorated function/class with the argument settings to use later."""
         try:
             arguments = fn._arguments
         except AttributeError:
             arguments = fn._arguments = list()
         arguments.append((args, kwargs))
         return fn
-    return decor
+    return _decor
 
 
 class PrettierTable(PrettyTable):
@@ -46,22 +68,55 @@ class PrettierTable(PrettyTable):
                 self.set_field_align(field, 'l')
 
 
-class TermtoolMetaclass(type):
+class _TermtoolMetaclass(type):
+
+    """Metaclass for `Termtool` classes.
+
+    This metaclass automatically sets the ``_subcommands`` class attribute to a
+    list of class attributes that have ``_subcommand`` attributes -- that is,
+    all the methods decorated with the `subcommand()` decorator.
+
+    """
 
     def __new__(cls, name, bases, attrs):
         attrs['_subcommands'] = [attr for attr in attrs.itervalues()
             if hasattr(attr, '_subcommand')]
-        return super(TermtoolMetaclass, cls).__new__(cls, name, bases, attrs)
+        return super(_TermtoolMetaclass, cls).__new__(cls, name, bases, attrs)
 
 
 class Termtool(object):
 
-    __metaclass__ = TermtoolMetaclass
+    """A terminal tool for performing actions at a command line.
+
+    Define a new terminal tool by subclassing `Termtool` and decorating its
+    instance methods with the `subcommand()` decorator. Use the `argument()`
+    decorator too to declare the subcommands' arguments. You can also use the
+    `argument()` decorator on your `Termtool` subclass to declare global
+    arguments valid for all commands.
+
+    Instantiate your class and call `run()` to run as a command line tool.
+
+    """
+
+    __metaclass__ = _TermtoolMetaclass
 
     progressbar = ProgressBar
     table = PrettierTable
 
     def write_config_file(self, *args):
+        """Write out a config file containing the given arguments.
+
+        On future command line runs of the tool, the saved command line
+        arguments will be used as though the user specified them on the command
+        line.
+
+        The arguments are written one per line to the ``~/.toolname`` file,
+        where `toolname` is the name of the `Termtool` instance's class, in
+        lower case. The file is created if not present and overwritten if it
+        is. The file is opened with umask 077 so it is readable only by the
+        user invoking the tool (and whose home directory it's written to).
+
+        """
         appname = type(self).__name__.lower()
         filepath = os.path.expanduser('~/.%s' % appname)
 
@@ -73,6 +128,13 @@ class Termtool(object):
                 config_file.write('\n')
 
     def read_config_file(self):
+        """Read the terminal user's config file for this tool.
+
+        The config file is ``~/.toolname`` where `toolname` is the name of the
+        `Termtool` instance's class, in lower case. The method returns a list
+        of config arguments as read from the file, one per line.
+
+        """
         appname = type(self).__name__.lower()
         filepath = os.path.expanduser('~/.%s' % appname)
         if not os.path.exists(filepath):
@@ -83,6 +145,20 @@ class Termtool(object):
         return config_args
 
     def main(self, argv):
+        """Perform the tool's functions, given the specified command line
+        arguments.
+
+        This method reads the instance's config file with the
+        `read_config_file()` method, adds the arguments specified in `argv`,
+        and performs the subcommand identified there.
+
+        The method returns an integer exit code suitable for using with
+        `sys.exit()`. That is, `main()` returns ``1`` if the command terminates
+        abnormally and ``0`` otherwise. However if ``--help`` or invalid
+        arguments are specified, the process is terminated by `argparse`
+        instead and `main()` will not return.
+
+        """
         config_args = self.read_config_file()
         args = config_args + argv
 
@@ -101,10 +177,13 @@ class Termtool(object):
 
         subparsers = parser.add_subparsers(dest='subcommand', title='subcommands', metavar='')
 
+        # Add all the subcommands in asciibetical order by command name.
         for command in sorted(self._subcommands, key=lambda c: c._subcommand[0]):
             name, about_args, about_kwargs = command._subcommand
             subparser = subparsers.add_parser(name, *about_args, **about_kwargs)
 
+            # Set the subparser's func so it becomes this command (callable)
+            # when the user invokes this subparser.
             subparser.set_defaults(func=command)
 
             try:
@@ -132,4 +211,11 @@ class Termtool(object):
         return 0
 
     def run(self):
+        """Invoke the terminal command for usage at the command line.
+
+        This method invokes the instance's `main()` method, passing the command
+        line arguments specified in `sys.argv`, and terminating the process
+        using `sys.exit()` with the exit code returned by `main()`.
+
+        """
         sys.exit(self.main(sys.argv[1:]))
